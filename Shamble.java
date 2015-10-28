45cc4bc5d65685c92a445393f1af07b929bb3ee8
PriestofTuna <lyttekk@yahoo.com>
2015-10-28 12:44:51 -0400

    Update Shamble.java

--- Shamble.java
+++ Shamble.java
@@ -1,43 +1,58 @@
 
-import java.util.Scanner;
-
-
-public class Peoples {
-public static void main(String[] args) {
-	Scanner keyboard = new Scanner(System.in);
-	String personsName = keyboard.next();
-	Persons person = new Persons();
-	Persons anotherPerson = new Persons("Bob", 24);
-	Persons Bob = new Person();
-	Persons.this.name = "Bob";
-	Persons.this.age = 24;
-	Persons.writeOutput();
-	
-	Persons Mat = new Person();
-	Persons.this.name = "Matthew";
-	Persons.this.age = "15";
-	
-}
-}
-// Everything above this comment is for the class "Peoples"
-// Everything below this comment is for the class Persons
 public class Persons {
 
 	public Persons(String name, int age) {
-		this.name = name;
-		this.age = age;
+		this.setName(name);
+		this.setAge(age);
+		// TODO Auto-generated constructor stub
 	}
 	
+	
+
 	private String name;
 	private int age;
-	
-	public String name() {
-		return this.name;
+
+	public String getName() {
+		return name;
 	}
-	public int age() {
-		return this.age;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 	public String toString() {
-		return "Person's name is : + " + this.name + "Age is" + this.age;
+		return "This Person's name is " + this.name + ": their Age is" + this.age;
 	}
+
+}
+// Everything above this comment is for the class "Persons"
+// Everything below this comment is for the class Peoples
+import java.util.Scanner;
+public class Peoples {
+
+	public Peoples() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner keyboard = new Scanner(System.in);
+    String personsName = keyboard.next();
+    Persons person = new Persons(personsName, 24);
+    Persons MarkPerson = new Persons("Mark", 15);
+    System.out.println(MarkPerson);
+    System.out.println(personsName);
+    System.out.println(person);
+    if (!MarkPerson.equals(personsName)){
+    System.out.print("This is not a spy with multiple identies, failure.");	
+    }
+	}
+
 }
