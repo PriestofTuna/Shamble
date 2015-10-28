60cd9a8039e52d0d045736f2c0889a93028a16da
PriestofTuna <lyttekk@yahoo.com>
2015-10-28 10:11:35 -0400

    Update Shamble.java

--- Shamble.java
+++ Shamble.java
@@ -1,30 +1,43 @@
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
-public class Shamble {
 
+public class Peoples {
 public static void main(String[] args) {
-    int numPeople =  Integer.parseInt(JOptionPane.showInputDialog("Please input the number of people "));
-    if (numPeople > 10) {
-        JOptionPane.showMessageDialog(null, numPeople/2);
-		    	
-	}else if ((numPeople >3) && (numPeople <=10)){
-				    	  
-	    JOptionPane.showMessageDialog(null, numPeople/3);
-					          
-	}else {
-	    JOptionPane.showMessageDialog(null, "The number of people has to be at least 3 ");
-						  }
-int numPlayers =  Integer.parseInt(JOptionPane.showInputDialog("Please input the number of players "));
-    if ((numPlayers > 11) && (numPlayers < 55)) {
-	    JOptionPane.showMessageDialog(null, numPlayers/11);
-	    JOptionPane.showMessageDialog(null, "number of players" + numPlayers );
-    }else {
-        JOptionPane.showMessageDialog(null, numPlayers/1);
-								    	        	
-													        	
-	}
-																
-    }
+	Scanner keyboard = new Scanner(System.in);
+	String personsName = keyboard.next();
+	Persons person = new Persons();
+	Persons anotherPerson = new Persons("Bob", 24);
+	Persons Bob = new Person();
+	Persons.this.name = "Bob";
+	Persons.this.age = 24;
+	Persons.writeOutput();
+	
+	Persons Mat = new Person();
+	Persons.this.name = "Matthew";
+	Persons.this.age = "15";
+	
 }
+}
+// Everything above this comment is for the class "Peoples"
+// Everything below this comment is for the class Persons
+public class Persons {
 
+	public Persons(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	private String name;
+	private int age;
+	
+	public String name() {
+		return this.name;
+	}
+	public int age() {
+		return this.age;
+	}
+	public String toString() {
+		return "Person's name is : + " + this.name + "Age is" + this.age;
+	}
+}
